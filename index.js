7ae4c58aaa42a7f70e0d6151dde2c67f59f0133e
Travis J. Webb <me@traviswebb.com>
2016-11-01 00:13:52 -0400

    [task] add graphql output capability

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const Trailpack = require('trailpack')
-const { graphql, buildSchema } = require('graphql')
-const { validate } = require('graphql/validation')
+const { buildSchema } = require('graphql')
 const graphqlTag = strings => strings.join()
+const fs = require('fs')
+const path = require('path')
 
 module.exports = class GraphqlTrailpack extends Trailpack {
 
@@ -15,36 +16,57 @@ module.exports = class GraphqlTrailpack extends Trailpack {
 
   configure () {
     const config = this.app.config.graphql
-    const models = this.app.models
-
-    this.gqlModels = Object.keys(models)
-      .map(k => [ models[k], k ])
-      .filter(([ model, modelName ]) => {
-        const config = model.constructor.config()
-        return config && config.type === 'graphql'
-      })
-
-    this.schemaString = this.gqlModels
-      .map(([ model, modelName ]) => model.constructor.schema(graphqlTag) || '')
-      .reduce((acc, gql) => acc + gql, '')
-
-    this.schema = buildSchema(this.schemaString)
 
     if (config.enableIntrospectionQuery !== false) {
       config.enableIntrospectionQuery = true
     }
+    this.schemaFile = path.resolve(this.app.config.main.paths.temp, 'schema.graphql')
   }
 
   /**
    * Compile graphql schema and resolvers
    */
   initialize () {
-    const config = this.app.config.graphql
     const models = this.app.models
 
-    this.resolvers = this.gqlModels
-      .map(([ model, modelName ]) => model.constructor.resolver(this.app) || { })
-      .reduce((root, resolvers) => Object.assign(root, resolvers), { })
+    const gqlModels = Object.keys(models)
+      .map(k => [ models[k], k ])
+      .filter(([ model, modelName ]) => {
+        const config = model.constructor.config()
+        return config && config.type === 'graphql'
+      })
+
+    const schemaString = gqlModels
+      .map(([ model, modelName ]) => model.constructor.schema(graphqlTag) || '')
+      .join('\n')
+
+    const rootQueryBody = gqlModels
+      .map(([ model, modelName ]) => `${modelName.toLowerCase()}: ${modelName}Query`)
+      .join('\n')
+
+    this.schemaString = `
+      ${schemaString}
+
+      type Query {
+        ${rootQueryBody}
+      }
+
+      schema {
+        query: Query
+      }
+    `
+
+    this.schema = buildSchema(this.schemaString)
+
+    this.resolvers = gqlModels
+      .map(([ model, modelName ]) => {
+        return {
+          [modelName.toLowerCase()]: model.constructor.resolver(this.app) || { }
+        }
+      })
+      .reduce((obj, resolver) => Object.assign(obj, resolver), { })
+
+    fs.writeFileSync(this.schemaFile, this.schemaString)
   }
 
   constructor (app) {
